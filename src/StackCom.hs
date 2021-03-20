5790ba816f85fcf5d4c96699f9b7df685b48eafd
jrp2014 <jrp2014@users.noreply.github.com>
2021-03-20 20:27:01 +0000

    Small optimization

--- src/StackCom.hs
+++ src/StackCom.hs
@@ -1,12 +1,18 @@
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE LambdaCase #-}
 
 module StackCom
-  ( projectName
-  ) where
+  ( projectName,
+  comp,
+  exec,
+  run,
+  fac
+  )
+where
 
-import           Control.Monad.State
-import           Data.List
-import           Data.Maybe
+import Control.Monad.State
+    ( evalState, execState, MonadState(put, get), State )
+import Data.Maybe ( fromMaybe )
 
 projectName :: String
 projectName = "StackCom"
@@ -21,12 +27,12 @@ data Prog
   deriving stock (Show)
 
 data Expr = Val Int | Var Name | App Op Expr Expr
-  deriving (Show)
+  deriving stock (Show)
 
 type Name = Char
 
 data Op = Add | Sub | Mul | Div
-  deriving (Show)
+  deriving stock (Show)
 
 -- Virtual machine
 
@@ -44,70 +50,85 @@ data Inst
   | JUMP Label
   | JUMPZ Label
   | LABEL Label
-  deriving (Show)
+  deriving stock (Show)
 
 type Label = Int
 
 --  Compiler
 
 comp :: Prog -> Code
-comp prog = evalState (comp' prog) 0
+comp prog = rewriteLabels $ evalState (comp' prog) 0
 
 comp' :: Prog -> State Label Code
-comp' (Assign name expr         ) = return $ compExpr expr ++ [POP name]
+comp' (Assign name expr) = return $ compExpr expr ++ [POP name]
 comp' (If expr thenProg elseProg) = do
   label <- get
   let label' = succ label
   put $ succ label'
   cthen <- comp' thenProg
   celse <- comp' elseProg
-  return
-    $  compExpr expr
-    ++ [JUMPZ label]
-    ++ cthen
-    ++ [JUMP label', LABEL label]
-    ++ celse
-    ++ [LABEL label']
+  return $
+    compExpr expr
+      ++ [JUMPZ label]
+      ++ cthen
+      ++ [JUMP label', LABEL label]
+      ++ celse
+      ++ [LABEL label']
 comp' (While expr prog) = do
   label <- get
   let label' = succ label
   put $ succ label'
   cprog <- comp' prog
-  return
-    $  [LABEL label]
-    ++ compExpr expr
-    ++ [JUMPZ label']
-    ++ cprog
-    ++ [JUMP label, LABEL label']
+  return $
+    [LABEL label]
+      ++ compExpr expr
+      ++ [JUMPZ label']
+      ++ cprog
+      ++ [JUMP label, LABEL label']
 comp' (Seq progs) = do
   code <- mapM comp' progs
   return $ concat code
 
 compExpr :: Expr -> Code
-compExpr (Val i     ) = [PUSH i]
-compExpr (Var name  ) = [PUSHV name]
+compExpr (Val i) = [PUSH i]
+compExpr (Var name) = [PUSHV name]
 compExpr (App op l r) = compExpr l ++ compExpr r ++ [DO op]
 
 type LabelTable = [(Label, Int)]
 
 labels :: Code -> LabelTable
-labels code = [ (l, i) | (LABEL l, i) <- zip code [0 ..] ]
+labels code = [(l, i) | (LABEL l, i) <- zip code [0 ..]]
+
+rewriteLabels :: Code -> Code
+rewriteLabels code = fmap rewriteJumps code
+  where
+    lt = labels code
+    rewriteJumps = \case
+      JUMP label ->
+        JUMP $
+          fromMaybe
+            (error $ "JUMP: label " ++ show label ++ " is not in defined")
+            (lookup label lt)
+      JUMPZ label ->
+        JUMPZ $
+          fromMaybe
+            (error $ "JUMPZ: label " ++ show label ++ " is not in defined")
+            (lookup label lt)
+      inst -> inst
 
 -- Machine
 
 data Machine = Machine
-  { pc    :: Int
-  , stack :: Stack
-  , mem   :: Mem
+  { pc :: Int,
+    stack :: Stack,
+    mem :: Mem
   }
-  deriving stock Show
+  deriving stock (Show)
 
 type StateMachine = State Machine ()
 
-
 updateMem :: Mem -> (Name, Int) -> Mem
-updateMem mem a@(name, value) = a : filter ((name /=) . fst) mem
-
+updateMem memory a@(name, _value) = a : filter ((name /=) . fst) memory
 
 -- Execution
 
@@ -125,60 +146,52 @@ eval code = do
 eval' :: Code -> StateMachine
 eval' code = do
   m <- get
-  let pcm    = pc m
+  let pcm = pc m
       stackm = stack m
-      memm   = mem m
+      memm = mem m
       labelt = labels code
 
   case code !! pcm of
-    (PUSH  int ) -> put $ Machine (succ pcm) (int : stackm) memm
-    (PUSHV name) -> put $ Machine
-      (succ pcm)
-      ( fromMaybe (error $ "PUSHV: " ++ show name ++ " is not in memory")
-                  (lookup name memm)
-      : stackm
-      )
-      memm
-    (POP name) -> put
-      $ Machine (succ pcm) (tail stackm) (updateMem memm (name, head stackm))
+    (PUSH int) -> put $ Machine (succ pcm) (int : stackm) memm
+    (PUSHV name) ->
+      put $
+        Machine
+          (succ pcm)
+          ( fromMaybe
+              (error $ "PUSHV: " ++ show name ++ " is not in memory")
+              (lookup name memm) :
+            stackm
+          )
+          memm
+    (POP name) ->
+      put $ Machine (succ pcm) (tail stackm) (updateMem memm (name, head stackm))
     (DO op) -> do
-      let x      = head stackm
-          y      = head $ tail stackm
+      let x = head stackm
+          y = head $ tail stackm
           stack' = tail $ tail stackm
       case op of
         Add -> put $ Machine (succ pcm) (y + x : stack') memm
         Sub -> put $ Machine (succ pcm) (y - x : stack') memm
         Mul -> put $ Machine (succ pcm) (y * x : stack') memm
         Div -> put $ Machine (succ pcm) (y `div` x : stack') memm
-    (JUMP label) -> put $ Machine
-      (succ
-        (fromMaybe (error $ "JUMP: LABEL " ++ show label ++ " not found")
-                   (lookup label labelt)
-        )
-      )
-      stackm
-      memm
-    (JUMPZ label) -> put $ Machine
-      (succ $ if head stackm == 0
-        then fromMaybe (error $ "JUMPZ: LABEL " ++ show label ++ " not found")
-                       (lookup label labelt)
-        else pcm
-      )
-      (tail stackm)
-      memm
-    (LABEL label) -> put $ Machine (succ pcm) stackm memm
+    (JUMP label) ->
+      put $ Machine (succ label) stackm memm
+    (JUMPZ label) ->
+      put $ Machine (succ $ if head stackm == 0 then label else pcm) (tail stackm) memm
+    (LABEL _label) -> put $ Machine (succ pcm) stackm memm
 
 -- Factorial example
 
 fac :: Int -> Prog
-fac n = Seq
-  [ Assign 'A' (Val 1)
-  , Assign 'B' (Val n)
-  , While
-    (Var 'B')
-    (Seq
-      [ Assign 'A' (App Mul (Var 'A') (Var 'B'))
-      , Assign 'B' (App Sub (Var 'B') (Val (1)))
-      ]
-    )
-  ]
+fac n =
+  Seq
+    [ Assign 'A' (Val 1),
+      Assign 'B' (Val n),
+      While
+        (Var 'B')
+        ( Seq
+            [ Assign 'A' (App Mul (Var 'A') (Var 'B')),
+              Assign 'B' (App Sub (Var 'B') (Val (1)))
+            ]
+        )
+    ]
